ad71084bdd74b4ffa2ee24660f592f6ea4a218de
Mateusz Piątkowski <quantzo@live.com>
2016-04-27 22:48:09 +0200

    Evaluation

--- script.R
+++ script.R
@@ -1,61 +1,106 @@
 testHMMPackage <- function() {
-    if (!("HMM" %in% rownames(installed.packages())))
-     {
+    if (!("HMM" %in% rownames(installed.packages()))) {
         print("Installing HMM package")
         install.packages(c("HMM"))
-         }
+    }
 
 }
 
 
 testJsonLitePackage <- function() {
-     if (!("jsonlite" %in% rownames(installed.packages())))
-      {
+    if (!("jsonlite" %in% rownames(installed.packages()))) {
         print("Installing jsonlite package")
         install.packages(c("jsonlite"))
-      }
     }
+}
 
 
-
+set.seed(2005);
 testHMMPackage()
 testJsonLitePackage()
 library(jsonlite)
 library(HMM)
 
 
-
-#Defintion of symbols used in HMM
+#Definition of symbols used in HMM
 AminoAcidSymbols <- c("G", "P", "D", "E", "K", "R", "H", "S", "T", "N", "Q", "A", "M", "Y", "W", "V", "I", "L", "F", "C")
 
-#Definotions of states used in HMM
-HelixStates <- c("H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
-CoilStates <- c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12")
-StrandStates <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9")
+#Definitions of states used in HMM
+HelixStates <- c("H1", "H2", "H3") #, "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
+CoilStates <- c("C1", "C2", "C3") #, "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12")
+StrandStates <- c("B1", "B2", "B3") #, "B4", "B5", "B6", "B7", "B8", "B9")
 AllStates <- c(HelixStates, CoilStates, StrandStates)
+
+
 
 #Transition Matrix from figure 1
 TransitionMatrix <- t(matrix(c(0, 0.25, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0.25, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.16, 0, 0, 0, 0, 0.18, 0.16, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0, 0, 0.25, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0.25, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.25), 36))
 
 #Hidden Markov Model initialization
-Markov <- initHMM(States = AllStates, Symbols = AminoAcidSymbols, transProbs = TransitionMatrix, startProbs = NULL, emissionProbs = NULL)
+Markov <- initHMM(States = AllStates, Symbols = AminoAcidSymbols, transProbs = NULL, startProbs = NULL, emissionProbs = NULL)
 
 
 #Loading Data to data frame
-
 dataFrame <- fromJSON("Data/jsonProteins.json", flatten = TRUE)
 
-#Baum Welch training
-train <- function(model, dataFrame) {
-    for (i in 1:nrow(dataFrame)) {
-        model <- baumWelch(hmm = model, observation = unlist(strsplit(dataFrame[i, 3], '')), maxIterations = 2)
-        }
+testRows <- sample(1:nrow(dataFrame), 2000);
+testSet <- dataFrame[testRows,]
 
-    return(model)
+trainingSet <- dataFrame[ - testRows,]
+
+
+#Preparing observation vector
+observationTraining <- unlist(strsplit(paste(trainingSet[, 2]), ''))
+
+#Baum Welch training
+train <- function(model, observation, iter) {
+    return(tmp <- baumWelch(hmm = model, observation = observation, maxIterations = iter))
 }
 
+#Predict
+cleanResultsVector <- function(resultsVector) {
+    return(gsub("[[:digit:]]", "", resultsVector))
+}
+predictHmm <- function(model, observation) {
+    return(cleanResultsVector(viterbi(model, observation)))
+}
+
+#Evaluation functions
+
+numOfCorrectPositions <- function(predVector, resultsVector) {
+    return(sum(predVector == resultsVector))
+}
+
+evaluateEntry <- function(dataSetEntry, model) {
+    firstStructureVector <- unlist(strsplit(dataSetEntry$firstStructure, ''))
+    secondaryStructureVector <- unlist(strsplit(dataSetEntry$secondaryStructure, ''))
+
+    totalNumber <- length(firstStructureVector)
+    predictedVector <- predictHmm(model, firstStructureVector)
+    correctNumber <- numOfCorrectPositions(predictedVector, secondaryStructureVector)
+
+    result <- list()
+    result$TotalNumber <- totalNumber
+    result$CorrectNumber <- correctNumber
+
+    return(result)
+}
+
+evaluateDataSet <- function(dataSet, model) {
+
+    result <- list()
+    result$TotalNumber <- 0
+    result$CorrectNumber <- 0
 
 
-#Virterbi training
+    for (i in 1:nrow(dataSet)) {
+        dataEntry <- dataSet[i,]
+        entryResult <- evaluateEntry(dataEntry, model)
+        result$TotalNumber <- entryResult$TotalNumber + result$TotalNumber
+        result$CorrectNumber <- entryResult$CorrectNumber + result$CorrectNumber
 
-#VirterbiTrainedHMM <- viterbiTraining(Markov, observation = c("G", "P", "D"), maxIterations = 5)
+    }
+
+    return(result$CorrectNumber / result$TotalNumber)
+
+}
