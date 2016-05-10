505b8a1950fa9d0219e749206386c995cce12ccc
Mateusz Piątkowski <quantzo@live.com>
2016-05-10 15:02:58 +0200

    Parallel solution

--- script.R
+++ script.R
@@ -1,37 +1,39 @@
-testHMMPackage <- function() {
-    if (!("HMM" %in% rownames(installed.packages()))) {
-        print("Installing HMM package")
-        install.packages(c("HMM"))
-    }
-
-}
-
-
-testJsonLitePackage <- function() {
-    if (!("jsonlite" %in% rownames(installed.packages()))) {
-        print("Installing jsonlite package")
-        install.packages(c("jsonlite"))
+#############Instaling and testing libraries#############
+testPackage <- function(name) {
+    if (!(name %in% rownames(installed.packages()))) {
+        print(paste("Installing ", name, " package"))
+        install.packages(c(name))
     }
 }
 
+testPackage("HMM")
+testPackage("jsonlite")
+testPackage("foreach")
+testPackage("doParallel")
 
-set.seed(2005);
-testHMMPackage()
-testJsonLitePackage()
 library(jsonlite)
 library(HMM)
+library(foreach)
+library(doParallel)
+
+#############Cluster initialization#############
+initializeCluster <- function(numberOfCores) {
+    cl <- makeCluster(numberOfCores)
+    clusterSetRNGStream(cl, 9956)
+    registerDoParallel(cl)
+    return(cl)
+}
 
 
+#############Definitions#############
 #Definition of symbols used in HMM
 AminoAcidSymbols <- c("G", "P", "D", "E", "K", "R", "H", "S", "T", "N", "Q", "A", "M", "Y", "W", "V", "I", "L", "F", "C")
 
-#Definitions of states used in HMM
-HelixStates <- c("H1", "H2", "H3") #, "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
-CoilStates <- c("C1", "C2", "C3") #, "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12")
-StrandStates <- c("B1", "B2", "B3") #, "B4", "B5", "B6", "B7", "B8", "B9")
+#Definitions of states used in HMM from figure 1
+HelixStates <- c("H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15")
+CoilStates <- c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12")
+StrandStates <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9")
 AllStates <- c(HelixStates, CoilStates, StrandStates)
-
-
 
 #Transition Matrix from figure 1
 TransitionMatrix <- t(matrix(c(0, 0.25, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0.25, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0.75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.16, 0, 0, 0, 0, 0.18, 0.16, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0, 0, 0.25, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0.25, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.375, 0, 0.25, 0, 0.375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.75, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.25), 36))
@@ -39,82 +41,132 @@ TransitionMatrix <- t(matrix(c(0, 0.25, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, 0
 #Hidden Markov Model initialization
 Markov <- initHMM(States = AllStates, Symbols = AminoAcidSymbols, transProbs = NULL, startProbs = NULL, emissionProbs = NULL)
 
-
+#############Data loading#############
 #Loading Data to data frame
 dataFrame <- fromJSON("Data/jsonProteins.json", flatten = TRUE)
 
-testRows <- sample(1:nrow(dataFrame), 2000);
-testSet <- dataFrame[testRows,]
+#############Preparing training and test data sets#############
+testRows <- sample(1:nrow(dataFrame), nrow(dataFrame)/3);
 
-trainingSet <- dataFrame[ - testRows,]
+trainingSet <- dataFrame[testRows,]
+testSet <- dataFrame[ - testRows,]
 
+#############Preparing observation vector for learning#############
+observationTraining <- unlist(strsplit(paste(trainingSet[, c("firstStructure")]), ''))
 
-#Preparing observation vector
-observationTraining <- unlist(strsplit(paste(trainingSet[, 2]), ''))
-
-#Baum Welch training
+#############Baum Welch training#############
 train <- function(model, observation, iter) {
     return(tmp <- baumWelch(hmm = model, observation = observation, maxIterations = iter))
 }
-
-#Predict
+#############Predicting results form model#############
+#Funtion that cleans results vector from digits
 cleanResultsVector <- function(resultsVector) {
     return(gsub("[[:digit:]]", "", resultsVector))
 }
+
+#Fuction that returns clean results vector
 predictHmm <- function(model, observation) {
     return(cleanResultsVector(viterbi(model, observation)))
 }
 
-#Evaluation functions
-
-numOfCorrectPositions <- function(predVector, resultsVector) {
-    return(sum(predVector == resultsVector))
+#############Evaluation functions#############
+correctPositions <- function(predictedVector, labeledVector) {
+    return(predictedVector[labeledVector == predictedVector])
 }
+
+numberOfStates <- function(vector, stateSymbol) {
+    return(length(which(vector == stateSymbol)))
+}
+
+
 
 evaluateEntry <- function(dataSetEntry, model) {
     firstStructureVector <- unlist(strsplit(dataSetEntry$firstStructure, ''))
     secondaryStructureVector <- unlist(strsplit(dataSetEntry$secondaryStructure, ''))
 
     totalNumber <- length(firstStructureVector)
+    totalHelixesNumber <- numberOfStates(secondaryStructureVector, "H")
+    totalStrandsNumber <- numberOfStates(secondaryStructureVector, "B")
+    totalCoilsNumber <- numberOfStates(secondaryStructureVector, "C")
+
     predictedVector <- predictHmm(model, firstStructureVector)
-    correctNumber <- numOfCorrectPositions(predictedVector, secondaryStructureVector)
+
+    correctVector <- correctPositions(predictedVector, secondaryStructureVector)
+
+    correctNumber <- length(correctVector)
+
+    correctHelixesNumber <- numberOfStates(correctVector, "H")
+    correctStrandsNumber <- numberOfStates(correctVector, "B")
+    correctCoilsNumber <- numberOfStates(correctVector, "C")
 
     result <- list()
+
     result$TotalNumber <- totalNumber
     result$CorrectNumber <- correctNumber
+
+    result$TotalHelixesNumber <- totalHelixesNumber
+    result$CorrectHelixesNumber <- correctHelixesNumber
+
+    result$TotalStrandsNumber <- totalStrandsNumber
+    result$CorrectStrandsNumber <- correctStrandsNumber
+
+    result$TotalCoilsNumber <- totalCoilsNumber
+    result$CorrectCoilsNumber <- correctCoilsNumber
+
+
+    return(result)
+}
+combineEntriesResult <- function(ob1, ob2) {
+    result <- list()
+
+    result$TotalNumber <- ob1$TotalNumber + ob2$TotalNumber
+    result$CorrectNumber <- ob1$CorrectNumber + ob2$CorrectNumber
+
+    result$TotalHelixesNumber <- ob1$TotalHelixesNumber + ob2$TotalHelixesNumber
+    result$CorrectHelixesNumber <- ob1$CorrectHelixesNumber + ob2$CorrectHelixesNumber
+
+    result$TotalStrandsNumber <- ob1$TotalStrandsNumber + ob2$TotalStrandsNumber
+    result$CorrectStrandsNumber <- ob1$CorrectStrandsNumber + ob2$CorrectStrandsNumber
+     
+    result$TotalCoilsNumber <- ob1$TotalCoilsNumber + ob2$TotalCoilsNumber
+    result$CorrectCoilsNumber <- ob1$CorrectCoilsNumber + ob2$CorrectCoilsNumber
 
     return(result)
 }
 
 evaluateDataSet <- function(dataSet, model) {
-
-    result <- list()
-    result$TotalNumber <- 0
-    result$CorrectNumber <- 0
-
-
-    for (i in 1:nrow(dataSet)) {
-        dataEntry <- dataSet[i,]
-        entryResult <- evaluateEntry(dataEntry, model)
-        result$TotalNumber <- entryResult$TotalNumber + result$TotalNumber
-        result$CorrectNumber <- entryResult$CorrectNumber + result$CorrectNumber
-
+    results <- foreach(i = 1:nrow(dataSet), .combine = 'combineEntriesResult', .export = c('evaluateEntry', 'numberOfStates', 'correctPositions', 'predictHmm', 'cleanResultsVector', 'combineEntriesResult'), .packages = c("HMM")) %dopar% {
+        evaluateEntry(dataSet[i,], model)
     }
-
-    return(result$CorrectNumber / result$TotalNumber)
-
+    return(results)
 }
 
-performTest <- function(test, symbols, observationTraining) {
-    Helix <- c("H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15","H16", "H17", "H18", "H19", "H20", "H21", "H22", "H23", "H24", "H25", "H26", "H27", "H28", "H29", "H30", "H31", "H32", "H33", "H34", "H35", "H36", "H37", "H38", "H39", "H40", "H41", "H42", "H43", "H44", "H45", "H46", "H47", "H48", "H49", "H50", "H51", "H52", "H53", "H54", "H55", "H56", "H57", "H58", "H59", "H60", "H61", "H62", "H63", "H64", "H65", "H66", "H67", "H68", "H69", "H70", "H71", "H72", "H73", "H74", "H75", "H76", "H77", "H78", "H79", "H80", "H81", "H82", "H83", "H84", "H85", "H86", "H87", "H88", "H89", "H90", "H91", "H92", "H93", "H94", "H95", "H96", "H97", "H98", "H99", "H100") 
-    Coil<- c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22", "C23", "C24", "C25", "C26", "C27", "C28", "C29", "C30", "C31", "C32", "C33", "C34", "C35", "C36", "C37", "C38", "C39", "C40", "C41", "C42", "C43", "C44", "C45", "C46", "C47", "C48", "C49", "C50", "C51", "C52", "C53", "C54", "C55", "C56", "C57", "C58", "C59", "C60", "C61", "C62", "C63", "C64", "C65", "C66", "C67", "C68", "C69", "C70", "C71", "C72", "C73", "C74", "C75", "C76", "C77", "C78", "C79", "C80", "C81", "C82", "C83", "C84", "C85", "C86", "C87", "C88", "C89", "C90", "C91", "C92", "C93", "C94", "C95", "C96", "C97", "C98", "C99", "C100")
-    Strand <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15","B16", "B17", "B18", "B19", "B20", "B21", "B22", "B23", "B24", "B25", "B26", "B27", "B28", "B29", "B30", "B31", "B32", "B33", "B34", "B35", "B36", "B37", "B38", "B39", "B40", "B41", "B42", "B43", "B44", "B45", "B46", "B47", "B48", "B49", "B50", "B51", "B52", "B53", "B54", "B55", "B56", "B57", "B58", "B59", "B60", "B61", "B62", "B63", "B64", "B65", "B66", "B67", "B68", "B69", "B70", "B71", "B72", "B73", "B74", "B75", "B76", "B77", "B78", "B79", "B80", "B81", "B82", "B83", "B84", "B85", "B86", "B87", "B88", "B89", "B90", "B91", "B92", "B93", "B94", "B95", "B96", "B97", "B98", "B99", "B100") 
-    for (i in 1:100) {
-        states <- c(Helix[1:i], Coil[1:i], Strand[1:i])
-        model <- initHMM(States = states, Symbols = symbols, transProbs = NULL, startProbs = NULL, emissionProbs = NULL)
-        x <- train(model, observationTraining[1:15000], 100)
-        res <- evaluateDataSet(test, x$hmm)
-        print( paste("| ", i, " | ", i, " | ", i , " | ", res, " |"))
-    }
-    
+
+performUniformGrowthTest <- function(testSet, symbols, learningObservations, maxStatesNumber, modelStartProbs = NULL, learningIterations = 100, numberOfCores = 1) {
+    cl <- initializeCluster(numberOfCores)
+     testResults <- foreach(i = 1:maxStatesNumber, .combine = 'cbind', .export = c('uniformGrowthTestEntry', 'generateAllStatesVector', 'generateStateVector', 'train', 'evaluateDataSet', 'evaluateEntry', 'numberOfStates', 'correctPositions', 'predictHmm', 'cleanResultsVector', 'combineEntriesResult' ), .packages = c("HMM", "foreach", "doParallel")) %dopar% {
+        uniformGrowthTestEntry(i, symbols, learningObservations, testSet, modelStartProbs, learningIterations)
+     }
+
+    stopCluster(cl)
+    return(testResults)    
+}
+
+generateAllStatesVector <- function(numberOfStates = 0, helixNumber = numberOfStates, strandNumber = numberOfStates, coilNumber = numberOfStates) {
+    return(c(generateStateVector("H", helixNumber), generateStateVector("B", strandNumber), generateStateVector("C", coilNumber)))
+}
+
+generateStateVector <- function(symbol, numberOfStates) {
+    return(paste(symbol, seq(1:numberOfStates), sep=""))
+}
+
+
+uniformGrowthTestEntry <- function(numberOfStates, symbols, learningObservations, testSet, modelStartProbs, learningIterations) {
+    states <- generateAllStatesVector(numberOfStates = numberOfStates)
+    initializedModel <- initHMM(States = states, Symbols = symbols, startProbs = modelStartProbs)
+    trainedModel <- train(initializedModel, learningObservations, learningIterations)
+    result <- evaluateDataSet(testSet, trainedModel$hmm)
+    result$TestType = "Uniform Growth"
+    result$NumberOfStates = numberOfStates
+    return(result)
 }
